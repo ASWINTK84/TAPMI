ce603f14ce388d77f3d5dc76013ddc6031b3f5c5
ASWINTK84 <tkaswin0911@gmail.com>
2025-08-13 22:46:52 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,89 @@
-import Image from "next/image";
+"use client"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { FiDownload } from "react-icons/fi";
+
+interface User {
+  _id: string;
+  name: string;
+  gender: string;
+  age: number;
+  pronouns: string;
+  profileImageUrl: string;
+  email: string;
+  phone: string;
+  resumeUrl: string;
+  
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+     const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/users");
+        const data: User[] = await res.json(); 
+        setUsers(data); 
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  console.log(users);
+  
+
+  if (loading) return <p>Loading...</p>;
+
+
+
+  return (
+    <div className="min-h-screen bg-white ">
+
+      <main className="p-8 sm:p-10 grid grid-cols-1  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 place-items-center">
+        {users.map((profile) => (
+          
+            <div
+            key={profile._id}  className="bg-gray-50 rounded-2xl shadow-md overflow-hidden cursor-pointer hover:shadow-lg transition transform hover:scale-[1.03] w-[330px] pb-6"
+            >
+              <Link href={`/profile/${profile._id}`}>
+              <div className="bg-orange-400 flex justify-center items-center h-36 relative">
+                <img
+                  src={profile.profileImageUrl}
+                  alt={profile.name}
+                  className="w-38 h-38 rounded-full object-cover absolute -bottom-16 left-1/2 transform -translate-x-1/2 "
+                />
+              </div>
+              <div className="pt-20 text-center px-8">
+                <h2 className="text-gray-900 text-lg font-semibold mb-1 ">{profile.name}</h2>
+                <p className="text-gray-500 text-sm mb-4">
+                  {profile.gender} | {profile.age} | {profile.pronouns}
+                </p>
+              
+              </div>
+              </Link>
+
+              <div className="flex justify-center">
+                  <a
+                    href={profile.resumeUrl}
+                    className="flex items-center gap-2 px-6 py-3 w-[250px] rounded-full border border-white bg-gray-50 hover:bg-gray-100 transition shadow-sm"
+                  >
+                    
+                    <FiDownload className="w-5 h-5 text-gray-700" />
+                    <span className="text-gray-400 font-medium">Download My Resume</span>
+                  </a>
+                </div>
+            </div>
+          
+        ))}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+</div>
+
   );
 }
---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,325 @@
+"use client"
+
+import { FaEnvelope, FaPhone, FaDownload, FaPlay, FaGithub, FaLinkedin } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react"; 
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { FiDownload, FiMail, FiPhone } from "react-icons/fi";
+import { HiMail } from "react-icons/hi";
+import { IoPlayCircle, IoPlayCircleOutline } from "react-icons/io5";
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+
+interface Skill {
+  name: string;
+  iconUrl: string;
+} 
+interface VisualResume {
+  videoUrl?: string;  
+}
+interface SocialLink {
+  platform: string; 
+  url: string;
+}
+
+
+interface User {
+  _id: string;
+  name: string;
+  gender: string;
+  age: number;
+  pronouns: string;
+  profileImageUrl: string;
+  email: string;
+  phone: string;
+  resumeUrl: string;
+   selfIntroduction?: string[];
+  skills?: Skill[];
+ projects?: { title: string; description: string; imageUrl: string }[];
+caseStudies?: { title: string; description: string; imageUrl: string }[];
+visualResume?: VisualResume;
+socialLinks?: SocialLink[];
+
+}
+
+export default function ProfileHeader() {
+const params = useParams();
+  const userId = params.id;
+  const router = useRouter();
+const visualResumeRef = useRef<HTMLDivElement>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState("caseStudies"); 
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/api/users/${userId}`);
+        if (!res.ok) throw new Error("User not found");
+        const data: User = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <p>User not found</p>;
+
+
+  // Project or casestudy
+  const dataToShow = selectedCategory === "caseStudies" ? user.caseStudies : user.projects;
+
+console.log(dataToShow);
+
+
+
+  const scrollToVisualResume = () => {
+    visualResumeRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+
+
+  return (
+    <div className="w-full ">
+      <div className="bg-gradient-to-r from-orange-400 to-red-500 h-75 relative">
+
+         {/* backbutton */}
+       <div className="mr-auto ml-5 ">
+                 <button
+                    onClick={() => router.push("/")} 
+                    className="bg-orange-300 text-black px-6 py-2 rounded-4xl hover:bg-orange-200 transition mt-6"
+                    >
+                    ← Back to Home
+                    </button>
+        </div>
+        
+        <div className="absolute bottom-4 left-10 flex items-center gap-6 text-white text-sm">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <FaEnvelope /> {user.email}
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <FaPhone /> {user.phone}
+          </div>
+        </div>
+
+        <a
+          href={user.resumeUrl }
+          className="absolute bottom-4 right-10 flex items-center gap-2 px-6 py-2 border border-white rounded-full text-white hover:bg-white hover:text-orange-500 transition"
+        >
+          <FaDownload /> Download My Resume
+        </a>
+      </div>
+
+      {/* Profile pic */}
+      <div className="flex justify-center -mt-45 relative z-10">
+        <div className="rounded-full border-4 border-white overflow-hidden w-90 h-90">
+          <img
+            src={user.profileImageUrl} 
+            alt="Profile"
+            className="w-full h-full object-cover"
+          />
+        </div>
+         
+      </div>
+
+   
+
+      {/* Details */}
+      <div className="text-center mt-10">
+        
+        <h1 className="text-4xl text-black font-bold">{user.name}</h1>
+        <p className="text-gray-600 text-xl font-bold">{user.gender} | {user.age} | {user.pronouns}</p>
+
+        {/* Watch visual resume */}
+        <button className="mt-8 flex items-center gap-2 mx-auto pe-8   rounded-full bg-orange-500 text-white hover:bg-orange-600 transition"
+            onClick={scrollToVisualResume}
+        >
+          <IoPlayCircle size={50} /> Watch my Visual Resume Now
+        </button>
+
+      </div>
+      <div className="my-9 flex justify-between items-center border border-gray-200 rounded-4xl px-8 py-8 max-w-6xl mx-auto  shadow-sm">
+           
+            <span className="text-sm font-semibold text-gray-500 cursor-pointer">
+                Core Skills & Technical Proficiencies
+            </span>
+            <span className="text-sm font-semibold text-gray-500 cursor-pointer">
+                Professional Journey & Internship Roles
+            </span>
+            <span className="text-sm font-semibold text-gray-500 cursor-pointer">
+                Case Insights & Key Projects
+            </span>
+            <span className="text-sm font-semibold text-gray-500 cursor-pointer">
+                Learning & Academic Milestones
+            </span>
+            <span className="text-sm font-semibold text-gray-500 cursor-pointer">
+                Endorsements from Mentors & Peers
+            </span>
+        </div>
+
+
+        <div className="max-w-6xl mx-auto text-xl text-center text-gray-600 space-y-6 leading-relaxed px-4 py-8">
+            {
+            user.selfIntroduction?.map((intro, i) => (
+                 <p key={i}>{intro}</p>
+            ))
+            }
+        </div>
+
+
+        <div className="flex justify-center items-center gap-12 py-8 bg-white">
+            
+            {
+                user.skills?.map((skill, i)=> (
+                    <img key={i} src={skill.iconUrl} alt={skill.name} className="h-30 opacity-25" />
+                ))
+            }
+         </div>
+
+
+         <div className="max-w-6xl mx-auto py-12 px-4 relative">
+      <h2 className="text-4xl font-bold text-gray-800 mb-8 max-w-80">
+        Case Insights & Key Projects
+      </h2>
+
+      {/* // Project or casestudy */}
+      <div className="absolute top-20 right-0 flex space-x-2 border rounded-3xl p-1">
+        <button
+          className={`px-4 py-2 rounded-3xl font-medium transition ${
+            selectedCategory === "caseStudies"
+              ? "bg-gray-300 text-white"
+              : "bg-transparent text-gray-700 hover:bg-gray-200"
+          }`}
+          onClick={() => setSelectedCategory("caseStudies")}
+        >
+          Case Studies
+        </button>
+        <button
+          className={`px-4 py-2 rounded-3xl font-medium transition ${
+            selectedCategory === "projects"
+              ? "bg-gray-300 text-white"
+              : "bg-transparent text-gray-700 hover:bg-gray-200"
+          }`}
+          onClick={() => setSelectedCategory("projects")}
+        >
+          Projects
+        </button>
+      </div>
+
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={30}
+        slidesPerView={1}
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
+       
+      >
+        {dataToShow?.map((item, index) => (
+          <SwiperSlide
+                key={index}
+                className="bg-white border rounded-xl shadow p-6 hover:shadow-lg transition flex flex-col items-center justify-between"
+                >
+                <div className="w-full h-40 overflow-hidden rounded-md mb-4">
+                    <img
+                    src={item.imageUrl}
+                    alt={item.title}
+                    className="w-full h-full object-cover"
+                    />
+                </div>
+
+                <h3 className="font-semibold text-sm text-orange-500 mb-2 text-center">{item.title}</h3>
+                
+                <p className="text-gray-600 text-sm text-center line-clamp-3">
+                    {item.description}
+                </p>
+                </SwiperSlide>
+
+        ))}
+      </Swiper>
+    </div>
+         
+         
+        <div className=" p-5 rounded-[15px] text-center font-sans"  ref={visualResumeRef}>
+        <h2 className="my-5 text-5xl font-bold text-gray-800">Visual Resume</h2>
+        <div className="mt-20 relative w-full max-w-5xl mx-auto rounded-[15px] overflow-hidden shadow-md">
+            <div className="aspect-video">
+            <iframe
+                className="w-full h-full"
+                src={user.visualResume?.videoUrl}
+                title="Visual Resume Video"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+            </div>
+            
+        </div>
+        </div>
+
+
+            <div className="bg-gray-100 p-10 mt-40 flex flex-col items-center font-sans">
+            <div className="mt-10 bg-gradient-to-r from-orange-400 to-red-600 text-white p-18 rounded-3xl text-center mb-8 shadow-lg  w-full">
+                <h2 className="text-4xl font-bold mb-5">Connect with {user.name}</h2>
+
+                <a href={user.resumeUrl }  >
+                <button className="bg-white text-orange-500 border-none px-8 py-4 rounded-3xl text-lg font-semibold cursor-pointer inline-flex items-center shadow-md hover:bg-gray-100 transition">
+
+                    <FiDownload className="me-3" size={20} color="#f44336" />
+                Download My Resume
+                </button>
+                </a>
+            </div>
+
+            <div className="flex justify-between max-w-6xl w-full m-5 flex-wrap">
+    {/* Left */}
+            <div className="flex gap-10">
+                <div className="flex flex-col items-center text-gray-600 text-sm">
+                <HiMail size={24} style={{ marginBottom: '5px' }} />
+                Email 
+                </div>
+                <div className="flex flex-col items-center text-gray-600 text-sm">
+                <FiPhone size={24} style={{ marginBottom: '5px' }} />
+                Phone
+                </div>
+            </div>
+
+ {/* Right */}
+            <div className="flex gap-10">
+                <div className="flex items-center gap-3 text-gray-600 border border-gray-300 rounded-full px-4 py-1 text-sm">
+                <a href={user.socialLinks?.github}><FaGithub size={20} /></a>
+                GitHub
+                </div>
+                <div className="flex items-center gap-3 text-gray-600 border border-gray-300 rounded-full px-4 py-1 text-sm">
+                <a href={user.socialLinks?.linkedin}><FaLinkedin size={20} /></a>
+                LinkedIn
+                </div>
+            </div>
+            </div>
+
+
+            <hr className="w-4/5 border-t border-gray-200 my-10" />
+
+  
+        </div>
+
+
+
+
+    </div>
+  );
+}
